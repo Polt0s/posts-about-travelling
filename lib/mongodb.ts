7175baca3f044d41b91ec513423b40349cd8cfb7
polt0s <poltos.pro@gmail.com>
2023-01-02 19:49:27 +0400

    Fix const

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -19,12 +19,12 @@ if (!URI) {
 
 if (process.env.NODE_ENV === 'development') {
     if (!global._mongoClientPromise) {
-        client = new MongoClient(uri, options);
+        client = new MongoClient(URI, options);
         global._mongoClientPromise = client.connect();
     }
     clientPromise = global._mongoClientPromise;
 } else {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(URI, options);
     clientPromise = client.connect();
 }
 
