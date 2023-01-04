618ed821dcc2774b0d0e9a66289d78dc7e7f52fc
polt0s <poltos.pro@gmail.com>
2023-01-04 23:33:08 +0400

    Create new component Header

---
+++ components/Header/Header.component.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router';
+
+import { CreatePost } from '../Post';
+
+import styles from './Header.module.css';
+
+export const Header = () => {
+    const router = useRouter();
+    return (
+        <header className={styles['Header']}>
+            {router.asPath === '/' && (
+                <CreatePost tag="create" />
+            )}
+        </header>
+    );
+};
