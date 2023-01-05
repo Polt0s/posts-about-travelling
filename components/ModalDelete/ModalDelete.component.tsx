70ddaa1f3b5648e1aebacec795b34f244b57a2f0
polt0s <poltos.pro@gmail.com>
2023-01-05 20:23:00 +0400

    Create a new component ModalDelete and add fake avatar

---
+++ components/ModalDelete/ModalDelete.component.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import {
+    Button,
+    Typography,
+    Modal,
+    Row
+} from 'antd';
+
+import styles from './ModalDelete.module.css';
+
+interface IDeletePost {
+    id: string;
+    func: (id: string) => Promise<void>;
+    content: string;
+}
+
+export const ModalDelete = ({ id, func, content }: IDeletePost): JSX.Element => {
+    const [openModal, setOpenModal] = useState<boolean>(false);
+
+    const onDeletePost = async () => {
+        await func(id);
+        setOpenModal(false);
+    };
+
+    return (
+        <>
+            <Typography.Text className={styles['Text-position']} onClick={() => setOpenModal(true)}>
+                Delete
+            </Typography.Text>
+
+            <Modal
+                open={openModal}
+                centered
+                onCancel={() => setOpenModal(false)}
+                footer={[
+                    <Button
+                        type="default"
+                        key="back"
+                        onClick={() => setOpenModal(false)}
+                        size="large"
+                    >
+                        Cancel
+                    </Button>,
+                    <Button
+                        type="primary"
+                        danger
+                        key="submit"
+                        onClick={onDeletePost}
+                        size="large"
+                    >
+                        Delete post
+                    </Button>
+                ]}
+            >
+                <Row justify="center">
+                    <Typography.Text className={styles['Content']}>
+                        {content}
+                    </Typography.Text>
+                </Row>
+            </Modal>
+        </>
+    );
+};
